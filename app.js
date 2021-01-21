4abf658e9e766565f71dc11fe29f0b4975da6e93
fitgeek96 <fitgeek96@gmail.com>
2021-01-21 15:43:40 +0100

    Next, Prev & progress

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const titleEl = document.querySelector('#title');
 const coverEl = document.querySelector('#cover');
 
 // SONG TITLES
-const songs = ['El-Ghorba', 'Liberte', 'Lotf-Allah-El-Khafi', 'Machafouhach'];
+const songs = ['El Ghorba', 'Liberte', 'Lotf Allah El Khafi', 'Machafouhach'];
 
 // Keep Track of song
 let songIndex = 2;
@@ -28,20 +28,60 @@ function loadSong(song) {
 }
 
 // Play song
-function playSong(){
+function playSong() {
     musicContainerEl.classList.add('play');
     playBtnEl.querySelector('i.fas').classList.remove('fa-play');
     playBtnEl.querySelector('i.fas').classList.add('fa-pause');
     audioEl.play();
 }
 
-function pauseSong(){
+function pauseSong() {
     musicContainerEl.classList.remove('play');
     playBtnEl.querySelector('i.fas').classList.remove('fa-pause');
     playBtnEl.querySelector('i.fas').classList.add('fa-play');
     audioEl.pause();
 }
 
+// Previous Song
+function prevSong() {
+    songIndex--;
+
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Previous Song
+function nextSong() {
+    songIndex++;
+
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+
+// update progress bar 
+function updateProgress(e){
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration ) * 100;
+    progressBarEl.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e){
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audioEl.duration;
+
+    audioEl.currentTime = (clickX / width) * duration;
+
+}
 
 // EVENT LISTENERS
 playBtnEl.addEventListener('click', () => {
@@ -53,3 +93,9 @@ playBtnEl.addEventListener('click', () => {
         playSong();
     }
 });
+
+prevBtnEl.addEventListener('click', prevSong);
+nextBtnEl.addEventListener('click', nextSong);
+audioEl.addEventListener('timeupdate', updateProgress);
+progressContainerEl.addEventListener('click', setProgress);
+audioEl.addEventListener('ended', nextSong);    
